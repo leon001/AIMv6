a690a130109dd4e1905d51133470491fee09f6b0
davidgao <davidgao1001@gmail.com>
2016-02-19 05:05:48 +0800

    add dummy bootloader

---
+++ boot/arch/armv7a/bootmain.c
@@ -0,0 +1,36 @@
+/* Copyright (C) 2016 David Gao <davidgao1001@gmail.com>
+ *
+ * This file is part of AIMv6.
+ *
+ * AIMv6 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AIMv6 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
+#include <sys/types.h>
+
+#define FW_BASE	0x1FF00000
+
+void (*uart_puts)(const char *) = (void *)(FW_BASE + 0x04);
+int (*sd_read)(uint32_t, uint16_t, uint32_t) = (void *)(FW_BASE + 0x08);
+
+__attribute__ ((noreturn))
+int boot_main(void)
+{
+	uart_puts("BL: Hello\r\n");
+spin:
+	while (1);
+}
