3dd39228b13a8a1d607a2c5b76e889767fb12294
Gan Quan <coin2028@hotmail.com>
2016-05-16 21:50:52 +0800

    automake scripts for libbitmap

--- kern/lib/libbitmap/bitmap_weight.c
+++ kern/lib/libbitmap/bitmap_weight.c
@@ -35,10 +35,10 @@ __weak int __bitmap_weight(const unsigned long *bitmap, int bits)
 	int k, w = 0, lim = bits/BITS_PER_LONG;
 
 	for (k = 0; k < lim; k++)
-		w += hweight_long(bitmap[k]);
+		w += hweight(bitmap[k]);
 
 	if (bits % BITS_PER_LONG)
-		w += hweight_long(bitmap[k] & BITMAP_LAST_WORD_MASK(bits));
+		w += hweight(bitmap[k] & BITMAP_LAST_WORD_MASK(bits));
 
 	return w;
 }
