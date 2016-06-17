80c53708a36d2c22f8c59a3ad2ada06a2ad0921a
davidgao <davidgao1001@gmail.com>
2016-06-18 05:06:29 +0800

    add arm arch_irq

---
+++ include/aim/irq.h
@@ -0,0 +1,33 @@
+/* Copyright (C) 2016 David Gao <davidgao1001@gmail.com>
+ *
+ * This file is part of AIMv6.
+ *
+ * AIMv6 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AIMv6 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _IRQ_H
+#define _IRQ_H
+
+#include <arch-irq.h>
+
+#ifndef local_irq_save_and_disable
+#define local_irq_save_and_disable(flags) \
+	do { \
+		local_irq_save(flags); \
+		local_irq_disable(); \
+	} while (0)
+#endif
+
+#endif /* _IRQ_H */
+
